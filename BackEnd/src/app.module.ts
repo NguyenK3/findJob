d96d0e32d6a975119a424855e1e2d95e3095a82f
NguyenK3 <nh027471@gmail.com>
2024-09-17 15:14:10 +0700

    Change Folder

--- BackEnd/src/app.module.ts
+++ BackEnd/src/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { softDeletePlugin } from 'soft-delete-plugin-mongoose'
 import { CompanyModule } from './company/company.module';
 import { JobsModule } from './jobs/jobs.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { JobsModule } from './jobs/jobs.module';
     AuthModule,
     CompanyModule,
     JobsModule,
+    FilesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
