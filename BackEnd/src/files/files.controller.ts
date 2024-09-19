feeb1041eee053fb0b9e2585e3c6f8a8b0a88291
NguyenK3 <nh027471@gmail.com>
2024-09-19 09:40:38 +0700

    Arrange Folder, Validation File

--- BackEnd/src/files/files.controller.ts
+++ BackEnd/src/files/files.controller.ts
@@ -1,17 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors,
+  HttpStatus, ParseFilePipeBuilder
+} from '@nestjs/common';
 import { FilesService } from './files.service';
 import { CreateFileDto } from './dto/create-file.dto';
 import { UpdateFileDto } from './dto/update-file.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Public } from 'src/customize/customizeDecoration';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) { }
 
   @Post('upload')
+  @Public()
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+  uploadFile(@UploadedFile(
+    new ParseFilePipeBuilder()
+      .addFileTypeValidator({
+        fileType: /^(jpg|jpeg|png|image\/png|gif|txt|doc|docx|text\/plain|pdf|application\/pdf)$/i,
+      })
+      .addMaxSizeValidator({
+        maxSize: 1024 * 1024
+      })
+      .build({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
+      }),
+  ) file: Express.Multer.File) {
+    // console.log(file);
   }
 
   @Get()
