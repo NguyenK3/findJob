80884d3e8d6f0ff2319ae671bd3ad75bfdf8642c
NguyenK3 <nh027471@gmail.com>
2024-11-15 13:58:14 +0700

    Permission Admin

---
+++ FrontEnd/src/app/(admin)/admin/permission/page.tsx
@@ -0,0 +1,11 @@
+import PermissionAdmin from "@/components/admin/permission";
+
+const PermissionAdminPage = () => {
+    return (
+        <>
+            <PermissionAdmin />
+        </>
+    )
+}
+
+export default PermissionAdminPage;
---
+++ FrontEnd/src/app/(admin)/admin/role/page.tsx
@@ -0,0 +1,9 @@
+const RolePageAdmin = () => {
+  return (
+    <>
+        <h1>Role Page Admin</h1>
+    </>
+  )
+}
+
+export default RolePageAdmin;
