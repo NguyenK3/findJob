f0d62c961c8fce4bc6180c618ba11ca82aa7795a
NguyenK3 <nh027471@gmail.com>
2024-12-18 22:21:45 +0700

    Fix

--- FrontEnd/src/components/components/permission/app.admin.permissionTable.tsx
+++ FrontEnd/src/components/components/permission/app.admin.permissionTable.tsx
@@ -3,7 +3,7 @@ import { Box, Button, Container, Grid, IconButton, MenuItem, TextField, Typograp
 import { DataGrid, GridColDef, GridToolbar, GridPaginationModel } from "@mui/x-data-grid";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
-import PermissionModal from "./app.admin.PermissionModal";
+import PermissionModal from "./app.admin.permissionModal";
 
 const PermissionTable = () => {
   const [permissions, setPermissions] = useState<IPermission[]>([]);
